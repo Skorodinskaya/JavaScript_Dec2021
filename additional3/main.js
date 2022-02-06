0a166540c0c6b6c30f8dcaa1c244e18040a98b69
Skorodinskaya <skorodinskaya01@gmail.com>
2022-02-06 14:48:38 +0200

    additional task3 from classwork3

--- additional3/main.js
+++ additional3/main.js
@@ -875,3 +875,174 @@ document.write('</div>')
 document.write('<br> <hr>')
 document.write('<div>Additional task 3</div>')
 document.write('<br> <hr>')
+
+// 1. Створити пустий масив та :
+//     a. заповнити його 50 парними числами за допомоги циклу.
+//     b. заповнити його 50 непарними числами за допомоги циклу.
+//     c. Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
+// d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
+
+let arrayEven = [];
+
+for (let i = 0; i < 50; i++) {
+    if (i % 2 === 0) {
+        arrayEven.push(i);
+    }
+}
+console.log(arrayEven);
+
+
+let arrayOdd = [];
+for (let i = 0; i < 50; i++) {
+    if (i % 2 !== 0) {
+        arrayOdd.push(i);
+    }
+}
+console.log(arrayOdd);
+
+let arrayTwentyRandom = [];
+for (let i = 0; i < 20; i++) {
+    arrayTwentyRandom[i] = Math.floor(Math.random() * 21);
+}
+console.log(arrayTwentyRandom);
+
+let arrayDiapason = [];
+for (let i = 0; i < 20; i++) {
+    arrayDiapason[i] = Math.floor(Math.random() * (732 - 8) + 8);
+}
+console.log(arrayDiapason)
+
+// 2. Вивести за допомогою console.log кожен третій елемен
+for (let i = 0; i < arrayTwentyRandom.length; i += 3) {
+    console.log(arrayTwentyRandom[i])
+}
+
+// 3. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним.
+for (let i = 0; i < arrayTwentyRandom.length; i += 3) {
+    if (arrayTwentyRandom[i] % 2 === 0) {
+        console.log(arrayTwentyRandom[i])
+    }
+}
+
+// 4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх в новий масив
+let newArrayFourthTask = [];
+for (let i = 0; i < arrayTwentyRandom.length; i += 3) {
+    if (arrayTwentyRandom[i] % 2 === 0) {
+        newArrayFourthTask.push(arrayTwentyRandom[i]);
+    }
+}
+console.log(newArrayFourthTask);
+
+// 5. Вивести кожен елемент масиву, сусід справа якого є парним
+// EXAMPLE: [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ] -> Має бути виведено 1, 9, 56
+
+let arrayNeighbor = [1, 2, 3, 5, 7, 9, 56, 8, 67];
+for (let i = 0; i < arrayNeighbor.length; i++) {
+    if (arrayNeighbor[i] % 2 === 0) {
+        console.log(arrayNeighbor[i - 1])
+    }
+}
+
+// 6. Є масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. Обрахувати середній чек.
+
+let check = [100, 250, 50, 168, 120, 345, 188];
+let total = 0;
+for (let i = 0; i < check.length; i++) {
+    total += check[i];
+}
+let avg = total / check.length;
+console.log(avg.toFixed(2));
+
+// 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
+let arrayMultiplyFive = [...Array(10)].map(() => Math.floor(Math.random() * 11));
+console.log(arrayMultiplyFive);
+let arrayMultiplyFiveResult = []
+for (let i = 0; i < arrayMultiplyFive.length; i++) {
+    arrayMultiplyFiveResult.push(arrayMultiplyFive[i] * 5)
+}
+console.log(arrayMultiplyFiveResult);
+
+// 8. Створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо елемент є числом - додати його в інший масив.
+
+let mixedArray = [1, 2, true, 'hello', 5, false, 'world', 8];
+let mixedArrayOnlyNumbers = [];
+
+for (let i = 0; i < mixedArray.length; i++) {
+    if (typeof mixedArray[i] === 'number') {
+        mixedArrayOnlyNumbers.push(mixedArray[i])
+    }
+}
+console.log(mixedArrayOnlyNumbers);
+
+// - Дано 2 масиви з рівною кількістю об'єктів. З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+// Записати цей об'єкт в новий масив
+
+let usersWithCities = [];
+
+let usersWithId = [
+    {id: 1, name: 'vasya', age: 31, status: false},
+    {id: 2, name: 'petya', age: 30, status: true},
+    {id: 3, name: 'kolya', age: 29, status: true},
+    {id: 4, name: 'olya', age: 28, status: false}
+];
+
+let citiesWithId = [
+    {user_id: 3, country: 'USA', city: 'Portland'},
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+    {user_id: 4, country: 'USA', city: 'Miami'}
+];
+
+for (const user of usersWithId) {
+    for (const city of citiesWithId) {
+        if (user.id === city.user_id) {
+            user.address = city;
+            usersWithCities.push(user)
+        }
+    }
+}
+console.log(usersWithCities);
+
+// - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
+
+let arrayTenEven = [...Array(10)].map(() => Math.floor(Math.random() * 11));
+console.log(arrayTenEven);
+for (let i = 0; i < arrayTenEven.length; i++) {
+    if (arrayTenEven[i] % 2 === 0) {
+        console.log(arrayTenEven[i]);
+    }
+}
+
+// - Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
+let arrayTen = [2, 4, 5, 15, 7, 8, 1, 0, 3, 12];
+let arrayTenCopy = [];
+for (let i = 0; i < arrayTen.length; i++) {
+    arrayTenCopy.push(arrayTen[i])
+}
+console.log(arrayTenCopy);
+
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for зібрати всі букви в слово.
+let arrayForAbc = ['a', 'b', 'c'];
+let str = '';
+for (let i = 0; i < arrayForAbc.length; i++) {
+    str = str + arrayForAbc[i];
+}
+console.log(str);
+
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу while зібрати всі букви в слово.
+let arrayWhileAbc = ['a', 'b', 'c'];
+let str2 = '';
+let i = 0;
+while (i < arrayWhileAbc.length) {
+    str2 = str2 + arrayWhileAbc[i];
+    i++
+}
+console.log(str2);
+
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for of зібрати всі букви в слово.
+let arrayForOfAbc = ['a', 'b', 'c'];
+let str3 = '';
+for (const arrayForOfAbcElement of arrayForOfAbc) {
+    str3 = str3 + arrayForOfAbcElement;
+}
+console.log(str3);
