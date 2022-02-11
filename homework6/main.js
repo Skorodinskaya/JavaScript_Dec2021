380a5d50ccee48ed2a503a861000c8d03f0d62d4
Skorodinskaya <skorodinskaya01@gmail.com>
2022-02-11 22:49:19 +0200

    complete classwork 6

--- homework6/main.js
+++ homework6/main.js
@@ -71,8 +71,7 @@ const capitalize = (str) => {
     for (let i = 0; i < arr.length; i++) {
         arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
     }
-    const str2 = arr.join(' ');
-    return str2;
+    return  str2 = arr.join(' ');
 }
 
 let capTest = 'hello, how are you'
