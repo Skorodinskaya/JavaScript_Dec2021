d40ca77301e240f9afc777fa2a19a17bc7351703
Skorodinskaya <skorodinskaya01@gmail.com>
2022-02-16 23:42:15 +0200

    started classwork 7

---
+++ classwork7/main.js
@@ -0,0 +1,101 @@
+// - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+
+function Car(model, manufacture, year, maxSpeed, engineVolume) {
+    this.model = model;
+    this.manufacture = manufacture;
+    this.year = year;
+    this.maxSpeed = maxSpeed;
+    this.engineVolume = engineVolume;
+    this.drive = function () {
+        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`)
+    };
+    this.info = function () {
+        for (const carKey in this) {
+            if (typeof this[carKey] !== "function") {
+                console.log(`${carKey} - ${this[carKey]}`)
+            }
+        }
+    };
+    this.increaseMaxSpeed = function (newSpeed) {
+        this.maxSpeed = this.maxSpeed + newSpeed;
+        console.log(`New speed is ${this.maxSpeed}`)
+    };
+    this.changeYear = function (newValue) {
+        this.year = newValue;
+        console.log(`The new year of production is ${this.year}`)
+    };
+    this.addDriver = function (driver) {
+        console.log(this.driver = driver);
+    }
+}
+
+let car = new Car('bmw', 'Korea', '2011', 250, 100);
+console.log(car);
+car.drive();
+car.info();
+car.increaseMaxSpeed(50);
+car.changeYear(2022);
+car.addDriver({name: 'Ivan', age: '30', sex: 'male'});
+console.log(car);
+
+// - (Те саме, тільки через клас)
+// Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+
+class CarSecond {
+    constructor(model, manufacture, year, maxSpeed, engineVolume) {
+        this.model = model;
+        this.manufacture = manufacture;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.engineVolume = engineVolume;
+    }
+
+    drive() {
+        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`)
+    };
+
+    info() {
+        for (const carSecondKey in this) {
+            console.log(`${carSecondKey} - ${this[carSecondKey]}`)
+        }
+    };
+
+    increaseMaxSpeed(newSpeed) {
+        this.maxSpeed = newSpeed + this.maxSpeed;
+        console.log(`The new max speed is ${this.maxSpeed}`)
+    };
+
+    changeYear(newValue) {
+        this.year = newValue;
+        console.log(`The new year of production is ${this.year}`)
+    };
+
+    addDriver(driver) {
+        console.log(this.driver = driver);
+    }
+}
+
+const secondCar = new CarSecond('audi', 'USA', 2005, 200, 3);
+console.log(secondCar);
+secondCar.drive();
+secondCar.increaseMaxSpeed(20);
+secondCar.changeYear(2010);
+secondCar.addDriver({name: 'Iryna', age: 25, sex: 'female'});
+console.log(secondCar);
+
+// -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
+// Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+//     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
+//     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
+
+
