354b03ff3b2ae48152c6491bbc2b917c61bc2f2f
Skorodinskaya <skorodinskaya01@gmail.com>
2022-02-17 21:47:45 +0200

    one task from additional classwork 7

--- classwork7/main.js
+++ classwork7/main.js
@@ -139,3 +139,75 @@ for (const cinderella of cinderellasArray) {
 
 const findCinderella = cinderellasArray.find((value) => value.shoeSize === prince.shoeSize);
 console.log(findCinderella);
+
+// - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
+// Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
+
+// {
+//     id: 1,
+//         name: 'Leanne Graham',
+//     username: 'Bret',
+//     email: 'Sincere@april.biz',
+//     address: {
+//     street: 'Kulas Light',
+//         suite: 'Apt. 556',
+//         city: 'Gwenborough',
+//         zipcode: '92998-3874',
+//         geo: {
+//         lat: '-37.3159',
+//             lng: '81.1496'
+//     }
+// },
+//     phone: '1-770-736-8031 x56442',
+//         website: 'hildegard.org',
+//     company: {
+//     name: 'Romaguera-Crona',
+//         catchPhrase: 'Multi-layered client-server neural-net',
+//         bs: 'harness real-time e-markets'
+// }
+// }
+
+class User {
+    constructor(id, name, username, email, street, suite, city, zipcode, lng, lat, phone, website,companyName, catchPhrase, bs) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.address = {
+            street,
+            suite,
+            city,
+            zipcode,
+            geo: {
+                lat,
+                lng
+            }
+        }
+        this.phone = phone;
+        this.website = website;
+        this.company = {
+            name: companyName,
+            catchPhrase,
+            bs
+
+        }
+    }
+}
+
+let user = new User(1,
+    'Leanne Graham',
+    'Bret',
+    'Sincere@april.biz',
+    'Kulas Light',
+    'Apt. 556',
+    'Gwenborough',
+    '92998-3874',
+    '-37.3159',
+    '81.1496',
+    '1-770-736-8031 x56442',
+    'hildegard.org',
+    'Romaguera-Crona',
+    'Multi-layered client-server neural-net',
+    'harness real-time e-markets');
+
+console.log(user);
